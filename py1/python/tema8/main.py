1f8d4655dc5bef5cc08778dca6380d68736dd794
agustin1996ra <rodriguezalvarezagustin@gmail.com>
2022-10-03 19:51:54 -0300

    tema8 python

--- py1/python/tema8/main.py
+++ py1/python/tema8/main.py
@@ -1,21 +1,23 @@
 def main():
     usuarios = listar_usuarios()
-    print(usuarios)
+    for usuario in usuarios:
+        print(f'Usuario: {usuario}')
 
 
 def listar_usuarios():
     f = open('/etc/passwd', 'r')
-    datos = f.readline()
+    datos = f.readlines()
     f.close()
+    resultado = []
     for linea in datos:
         if linea[0] == '#':
             continue
         if linea[0] == '_':
             continue
 
-        partes = linea.split()
-        print(linea)
-        print(partes)
+        partes = linea.split(':')
+        resultado.append(partes[0])
+    return resultado
 
 
 if __name__ == '__main__':
