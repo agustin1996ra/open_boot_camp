70f1ec95c399010e7deebedcbba72866bf2f2737
agustin1996ra <rodriguezalvarezagustin@gmail.com>
2023-06-15 15:11:15 -0300

    Django: Formularios a partir de modelos

--- django/prueba/formularios_modelos/employees/views.py
+++ django/prueba/formularios_modelos/employees/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .forms import EmployeeForm
 
-# Create your views here.
+def index(request):
+    form = EmployeeForm()
+    return render(request, 'index.html', {'form': form})
--- django/prueba/formularios_modelos/formularios_modelos/settings.py
+++ django/prueba/formularios_modelos/formularios_modelos/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'formularios_modelos.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
